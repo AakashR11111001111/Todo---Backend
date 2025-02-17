6212414a73aa4f56a98fdf1cc95d462790c907f7
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2025-02-18 00:22:43 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const readline = require('readline');
-const fs = require("fs");
+const fs = require("node:fs");
+const { fileURLToPath } = require('node:url');
+const { log } = require('node:console');
 
 const r1 = readline.createInterface({
     input: process.stdin,
@@ -9,23 +11,69 @@ const r1 = readline.createInterface({
 
 let tasks = [];
 
-// Task add karne ka function
 const addnewTask = async () => {
-    r1.question("Enter your Task name: ", (ans) => {
+    r1.question("Enter your Task name: ", (ans)=>{
         tasks.push(ans);
-        
-        fs.appendFile("tasks.txt", ans + "\n", (err) => { 
-            if (err) {
-                console.log(chalk.redBright("Error adding task:", err));
-            } else {
-                console.log(chalk.bgYellowBright.black("Task Added Successfully\n--------------------------------------------------------"));
-            }
+        fs.appendFile("tasks.txt",ans+"\n", (err)=>{
+            if(err){
+                console.log(err);
+                r1.close();
+            }else{
+            console.log(chalk.bgYellowBright.black("Task Added Successfully\n--------------------------------------------------------"));
             main();
-        });
-    });
-};
+        }
+       })
+    })  
+}
+
+
+const readFile = async () => {
+    fs.readFile("tasks.txt", "utf-8", (err, data)=>{
+        if(err) console.log(err);
+        else if(data.trim() === ""){
+            console.log(chalk.bgRed.white("No Task Found\n------------------"));
+            main();
+        }
+        else{
+            console.log(chalk.yellowBright(`All Tasks are:\n${data}`));
+            main();
+        }
+    })
+} 
+
+
+const markComplete = async () => {
+    let task = [];
+    fs.readFile("tasks.txt","utf-8",(err, data)=>{
+        if(err) console.log(err);
+        else if(data.trim() === ""){
+            console.log(chalk.bgRed.white("No Task Found\n------------------"));
+            main();
+        }
+        else{
+            r1.question("Enter the name of task you want to Mark Completed: ",(ans)=>{
+                task = data.split("\n");
+                // console.log("You Choosed : ", ans);
+                let idx = ans - 1;
+                fs.readFile("tasks.txt","utf-8",(err, data)=>{
+                    if(err) console.log(err);
+                    else if(data.trim() === ""){
+                        console.log(chalk.redBright("No Such Task Found"));
+                    }
+                    else{
+                        let dataArr = data.split("\n");
+                        // console.log("Uour Target => ", dataArr[idx]);      
+                    }
+                }) 
+            });   
+        }        
+    })
+}
+
 
 async function main() {
+    console.log(chalk.greenBright("SELECT AN OPTION"));
+
     console.log(chalk.greenBright("1. Add a New Task"));
     console.log(chalk.greenBright("2. View a list of tasks"));
     console.log(chalk.greenBright("3. Mark a task as complete"));
@@ -41,21 +89,20 @@ async function main() {
             await addnewTask();
             break;
         case 2:
-            console.log("You choose 2: View a list of tasks");
+            await readFile();
             break;
         case 3:
-            console.log("You choose 3: Mark a task as complete");
+            await markComplete();
             break;
         case 4:
             console.log("You choose 4: Remove a task");
             break;
         case 5:
-            console.log("You choose 5: Exit");
+            console.log("Bye Byee🥲");
             r1.close();
             return;
         default:
             console.log(chalk.redBright("Invalid choice! Enter a number between 1 and 5.\n-------------------------------------------------"));
-            main();
     }
 }
 main();
